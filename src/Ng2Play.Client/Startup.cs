4e6739ff9ac804a8b55e2f44b990d8d969e32401
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-08-15 23:31:28 +0200

    C# static pages support

--- src/Ng2Play.Client/Startup.cs
+++ src/Ng2Play.Client/Startup.cs
@@ -1,37 +1,76 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Ng2Play.Client
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IHostingEnvironment env)
         {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
-
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                builder.AddUserSecrets();
             }
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
 
-            app.Run(async (context) =>
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // add CORS support (used during development)
+            services.AddCors();
+            // Add framework services.
+            services.AddMvc()
+                .AddJsonOptions(o =>
+                {
+                    o.SerializerSettings.Formatting = Formatting.Indented;
+                    o.SerializerSettings.ContractResolver =
+                        new CamelCasePropertyNamesContractResolver();
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.MapWhen(context =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var path = context.Request.Path.Value;
+                if (path.Contains("/api/")) return false;
+                return (!path.Contains("."));
+            }, aBranch =>
+            {
+                aBranch.Use((context, next) =>
+                {
+                    context.Request.Path = new PathString("/index.html");
+                    return next();
+                });
+                aBranch.UseStaticFiles();
             });
+            // allow CORS during development
+            if (env.IsDevelopment())
+            {
+                app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+            }
+            app.UseMvc();
         }
     }
 }
